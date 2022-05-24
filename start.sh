63a10e797475370fb89541df931e48c37027534c
NVS BUDDY <105271761+NVSBUDDY@users.noreply.github.com>
2022-05-25 02:08:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/NVSBUDDY/filev5.git /filev5
+  git clone https://github.com/NVSBUDDY/filev5.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /filev5
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
-cd /filev5
+cd /EvaMaria
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
